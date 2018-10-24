24fac3d91904a928ebb81a0aec1ed66ecc0160dc
Keiran <keirans@gmail.com>
2018-10-23 19:31:41 -0700

    incomplete webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
 const path = require('path');
 
 module.exports = {
-  entry: './client/src/index.js',
+  entry: path.resolve(__dirname, 'client/src/index.jsx'),
+  mode: 'development',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        include: path.resolve(__dirname, 'client/src/'),
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
   }
 };
