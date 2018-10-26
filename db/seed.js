ab2e90497ea098886c47dd376ff9bcf64821d182
Keiran <keirans@gmail.com>
2018-10-26 15:52:56 -0700

    reformat seed.js for better human readability

--- db/seed.js
+++ db/seed.js
@@ -3,32 +3,53 @@ const fs = require('fs');
 const faker = require('faker');
 const db = require('./connection.js');
 const moment = require('moment');
+// a series of query strings to send the database
+const createDatabase = 'CREATE DATABASE IF NOT EXISTS funding_stats;';
+const useDatabase = 'USE funding_stats;';
+const createTable = `CREATE TABLE IF NOT EXISTS campaigns (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                     pledged DECIMAL(11, 2),
+                     goal DECIMAL(11, 2),
+                     currency CHAR(3),
+                     backers INT,
+                     deadline VARCHAR(50),
+                     media VARCHAR(100),
+                     category VARCHAR(20),
+                     city VARCHAR(40),
+                     state CHAR(2),
+                     country VARCHAR(40));`;
 
-const queryString1 = 'CREATE DATABASE IF NOT EXISTS funding_stats;';
-const queryString2 = 'USE funding_stats;';
-const queryString3 = 'CREATE TABLE IF NOT EXISTS campaigns (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, pledged DECIMAL(11, 2), goal DECIMAL(11, 2), currency CHAR(3), backers INT, deadline VARCHAR(50), media VARCHAR(100), category VARCHAR(20), city VARCHAR(40), state CHAR(2), country VARCHAR(40));';
-const queryString4 = 'TRUNCATE campaigns;';
-const queryString5 = 'LOAD DATA INFILE \'/var/lib/mysql-files/seedData.txt\' INTO TABLE campaigns COLUMNS TERMINATED BY \'\,\' OPTIONALLY ENCLOSED BY \'"\' (pledged, goal, backers, media, category, city, state, currency, country, deadline);';
+const clearTable = 'TRUNCATE campaigns;';
+const writeData = `LOAD DATA INFILE \'/var/lib/mysql-files/seedData.txt\' INTO TABLE campaigns
+                   COLUMNS TERMINATED BY \'\,\' OPTIONALLY ENCLOSED BY \'"\'
+                   (pledged, goal, backers, media, category, city, state, currency, country, deadline);`;
 
-let fakeRows = '';
-const currCodes = ['USD', 'GBP', 'CAD', 'AUD', 'NZD', 'EUR', 'DKK', 'NOK', 'SEK', 'CHF', 'HKD', 'SGD', 'MXN', 'JPY'];
-const countries = ['United States', 'United Kingdom', 'Canada', 'Australia', 'New Zealand', 'Netherlands', 'Denmark', 'Ireland', 'Norway', 'Sweden', 'Germany', 'France', 'Spain', 'Italy', 'Austria', 'Belgium', 'Switzerland', 'Luxembourg', 'Hong Kong', 'Singapore', 'Mexico', 'Japan'];
+let fakeRows = ''; // a variable to contain my comma-separated data
+const currCodes = ['USD', 'GBP', 'CAD', 'AUD', 'NZD', 'EUR', 'DKK',
+                   'NOK', 'SEK', 'CHF', 'HKD', 'SGD', 'MXN', 'JPY']; // the currency codes accepted by Kickstand
 
-const dateGen = () => {
+const countries = ['United States', 'United Kingdom', 'Canada', 'Australia', 'New Zealand',
+                   'Netherlands', 'Denmark', 'Ireland', 'Norway', 'Sweden', 'Germany',
+                   'France', 'Spain', 'Italy', 'Austria', 'Belgium', 'Switzerland', 'Luxembourg',
+                   'Hong Kong', 'Singapore', 'Mexico', 'Japan']; // the accepted countries of origin for campaigns
+
+const dateGen = () => { // helper function to generate a random date on the appropriate format
   return moment(faker.date.future()).format('YYYY-MM-DD');
 }
 
 for (let i = 0; i < 100; i++) {
-  fakeRows += faker.fake("{{commerce.price}},{{commerce.price}},{{random.number}},{{image.image}},{{commerce.product}},{{address.city}},{{address.stateAbbr}},");
+  // assemble random comma-separated data strings
+  fakeRows += faker.fake(`{{commerce.price}},{{commerce.price}},{{random.number}},{{image.image}},
+                          {{commerce.product}},{{address.city}},{{address.stateAbbr}},`);
+  // append data not handled by faker directly
   fakeRows += currCodes[Math.floor(Math.random() * currCodes.length)];
   fakeRows += ',';
   fakeRows += countries[Math.floor(Math.random() * countries.length)];
   fakeRows += ',';
   fakeRows += dateGen();
-  fakeRows += '\n';
+  fakeRows += '\n'; // terminate each line with a newline
 }
 
-fs.writeFile('/var/lib/mysql-files/seedData.txt', fakeRows, (err) => {
+fs.writeFile('/var/lib/mysql-files/seedData.csv', fakeRows, (err) => { // write CSV
   if (err) {
     console.log(err);
     return;
@@ -42,6 +63,6 @@ db.connect((error) => {
   }
 });
 
-db.query(queryString1 + queryString2 + queryString3 + queryString4 + queryString5);
+db.query(createDatabase + useDatabase + createTable + clearTable + writeData); // write mock data .csv into db
 
 db.end();
