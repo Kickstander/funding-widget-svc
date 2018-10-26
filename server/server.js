e8f6c72eb82b7c3a2c97c1aa845dfbb2f293690b
Keiran <keirans@gmail.com>
2018-10-26 16:09:32 -0700

    refactor for human readability

--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@ const path = require('path');
 const mysql = require('mysql');
 const db = require('../db/connection.js');
 
-const statsQuery = 'USE funding_stats; SELECT pledged, goal, currency, backers, deadline, category, city, state, country FROM campaigns WHERE id = ?;';
+const statsQuery = `USE funding_stats; SELECT pledged, goal, currency, backers, 
+                    deadline, category, city, state, country FROM campaigns WHERE id = ?;`;
 const port = 3000;
 const app = express();
 
@@ -11,8 +12,10 @@ app.use(express.static(path.resolve(__dirname, '../../funding-widget/public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/campaigns/:campaignId/stats', (req, res) => { // handle requests for a given campaigns stats
-  db.query(statsQuery, [parseInt(req.params.campaignId)], (error, results) => {
+app.get('/campaigns/:campaignId/stats', (req, res) => { // handle requests for a given campaign's stats
+  let campaign = parseInt(req.params.campaignId); // parse out the campaign's unique id
+
+  db.query(statsQuery, [campaign], (error, results) => { // retrieve campaign stats from the database
     res.status(200).send(results[1][0]);
   });
 });
