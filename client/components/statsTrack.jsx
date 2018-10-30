571ad054eca630e4650e756802983eaeec18a39b
Keiran <keirans@gmail.com>
2018-10-30 12:32:41 -0700

    begin building a rough component

--- client/components/statsTrack.jsx
+++ client/components/statsTrack.jsx
@@ -1,21 +1,34 @@
-const Promise = require('bluebird');
+import Promise from 'bluebird';
+import Request from 'request';
+import React from 'react';
 
-const db = require('../../db/connection.js');
 
 const statsTrack = () => {
-  
-  return (
+  const campaignId = 1 + Math.floor(Math.random() * 100);
+  const getRequest = new Promise((resolve, reject) => {
+    Request.get(`/campaigns/${campaignId}/stats/`, (error, reponse, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(body);
+      }
+    });
+  });
+  getRequest.then(stats => (
     <div className="funding-tracker">
       <div className="pledged-amount">
+        {stats.pledged}
       </div>
       <div className="goal-amount">
+        {stats.goal}
       </div>
       <div className="backers">
+        {stats.backers}
       </div>
       <div className="deadline">
+        {stats.backers}
       </div>
-    </div>
-  );
+    </div>));
 };
 
 export default statsTrack;
