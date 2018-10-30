e2353741f472c0978d97a87ebd10d371cb5d11bf
Keiran <keirans@gmail.com>
2018-10-29 19:37:56 -0700

    fail to make headway

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,19 @@
 module.exports = {
-  extends: ['airbnb']
+  "plugins": [
+      "react"
+  ],
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true
+    }
+  },
+  "env": {
+    "es6":     true,
+    "browser": true,
+    "node":    true,
+    "mocha":   true
+  },
+  extends: ['airbnb', 'plugin:react/recommended']
 };
