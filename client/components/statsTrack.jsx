d28eb3a90db627e571150df303dc92d5f31fcdff
Keiran <keirans@gmail.com>
2018-10-30 14:31:59 -0700

    continue efforts to render information from database

--- client/components/statsTrack.jsx
+++ client/components/statsTrack.jsx
@@ -1,34 +1,65 @@
-import Promise from 'bluebird';
-import Request from 'request';
 import React from 'react';
+import Promise from 'bluebird';
+import $ from 'jquery';
 
 
-const statsTrack = () => {
-  const campaignId = 1 + Math.floor(Math.random() * 100);
-  const getRequest = new Promise((resolve, reject) => {
-    Request.get(`/campaigns/${campaignId}/stats/`, (error, reponse, body) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(body);
-      }
+class statsTrack extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      pledged: 0,
+      goal: 0,
+      backers: 0,
+      deadline: '',
+    };
+  }
+
+  componentDidMount() {
+    const campaignId = 1 + Math.floor(Math.random() * 100);
+    console.log(campaignId);
+    const promise = new Promise((resolve) => {
+      $.get(`campaigns/${campaignId}/stats`, (data) => {
+        console.log(`Ajax result: ${data}`);
+        resolve(data);
+      });
     });
-  });
-  getRequest.then(stats => (
-    <div className="funding-tracker">
-      <div className="pledged-amount">
-        {stats.pledged}
+
+    promise.then((stats) => {
+      console.log(stats);
+      this.setState({
+        pledged: stats.pledged,
+        goal: stats.goal,
+        backers: stats.backers,
+        deadline: stats.deadline,
+      });
+    }).catch((err) => {
+      console.log(err);
+    });
+  }
+
+  render() {
+    const { pledged } = this.state;
+    const { goal } = this.state;
+    const { backers } = this.state;
+    const { deadline } = this.state;
+    console.log(`${pledged} ${goal} ${backers} ${deadline}`);
+    return (
+      <div className="funding-tracker">
+        <div className="pledged-amount">
+          Pledge Amount {pledged}
+        </div>
+        <div className="goal-amount">
+          Intended Goal {goal}
+        </div>
+        <div className="backers">
+          Backers {backers}
+        </div>
+        <div className="deadline">
+          Deadline {deadline}
+        </div>
       </div>
-      <div className="goal-amount">
-        {stats.goal}
-      </div>
-      <div className="backers">
-        {stats.backers}
-      </div>
-      <div className="deadline">
-        {stats.backers}
-      </div>
-    </div>));
-};
+    );
+  }
+}
 
 export default statsTrack;
