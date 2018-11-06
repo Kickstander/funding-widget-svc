d35c7f60a1ebeaef4fdcfc36d27fc7a2c384f816
Keiran <keirans@gmail.com>
2018-11-06 14:48:36 -0800

    refactored styling to be more proxy-friendly

--- client/index.jsx
+++ client/index.jsx
@@ -11,7 +11,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className={style.app}>
+      <div className={style.statstrack}>
         <StatsTrack />
       </div>
     );
