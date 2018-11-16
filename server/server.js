6e583163bc672c24a8a21c1db5d994fccc48d708
pouyaghazvini <pouyaghazvini@gmail.com>
2018-11-15 17:16:30 -0800

    created working post request for CRUD api

--- server/server.js
+++ server/server.js
@@ -1,16 +1,18 @@
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
-const db = require('../db/connection');
+const db = require('../db/connection.js');
+
 
 const statsQuery = `USE funding_stats; SELECT pledged, goal, currency, backers, 
                     deadline, category, city, state, country FROM campaigns WHERE id = ?;`;
-const port = 3002;
+
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, '../public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 
 app.get('/:campaignId/stats', cors(), (req, res) => { // handle requests for a given campaign's stats
   const campaign = parseInt(req.params.campaignId, 10); // parse out the campaign's unique id
@@ -27,7 +29,35 @@ app.get('/:campaignId', (req, res) => { // handle requests for a given campaign'
   res.status(200).sendFile(path.resolve(__dirname, '../public/index.html'));
 });
 
-app.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Listening on port: ${port}`);
+app.post('/:campaignId/stats', (req, res) => {
+  const data = req.body;
+  db.Campaign.addCampaign(data.id, data.pledged, data.goal,
+    data.backers, data.media, data.category, data.city,
+    data.state, data.currency, data.country, data.deadline);
+  res.end();
 });
+
+// app.put('/api/:campaignId/stats', (req, res) => {
+//   const id = req.params.campaignId;
+//   db.updateCampaign(id, req.body, (err, result) => {
+//     if (err) {
+//       res.send(err);
+//     } else {
+//       res.send(result);
+//     }
+//   });
+// });
+
+// app.delete('/api/:campaignId/stats', (req, res) => {
+//   const id = req.params.campaignId;
+//   db.deleteCampaign(id, (err, result) => {
+//     if (err) {
+//       res.send(err);
+//     } else {
+//       res.send(result);
+//     }
+//   });
+// });
+
+
+module.exports = app;
