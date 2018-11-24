28eba31e76dc836ae9f6a24cc5b390c6f4972453
pouyaghazvini <pouyaghazvini@gmail.com>
2018-11-23 16:40:09 -0800

    finished seeding Cassandra db and succesfully wrote first queries

--- db/db_cassandra/connection_cassandra.js
+++ db/db_cassandra/connection_cassandra.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-console */
+/* eslint-disable consistent-return */
+
 const cassandra = require('cassandra-driver');
 
 const client = new cassandra.Client({ contactPoints: ['127.0.0.1'] });
