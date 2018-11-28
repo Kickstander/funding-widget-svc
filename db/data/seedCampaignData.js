9814cc45b0fcfd7ed17f62a13c5afc48f6510aa2
pouyaghazvini <pouyaghazvini@gmail.com>
2018-11-28 12:59:49 -0800

    seeded campaigns data on ec2 instance

--- db/data/seedCampaignData.js
+++ db/data/seedCampaignData.js
@@ -2,10 +2,9 @@ const fs = require('fs.promises');
 const path = require('path');
 const faker = require('faker');
 const db = require('../db_postgres/index.js');
-const dataLimit = 10 * 100000;
+const dataLimit = 100000;
 const campaigns = 100;
 const startTime = 1543622400; //DEC 1, 2018 AT 00:00:00
-let currentTime = new Date().getTime();
 
 
 const seedCampaignData = function seedCampaignData() {
@@ -14,7 +13,7 @@ const seedCampaignData = function seedCampaignData() {
     const author = `${faker.name.firstName()} ${faker.name.lastName()}`;
     const user = `${faker.name.firstName()} ${Math.floor(Math.random() * 1000)}`;
     const country = Math.floor(Math.random() * 22);
-    const desc = "Campaign description";
+    const desc = faker.lorem.sentences(2);
 
     for (let j = 0; j < campaigns; j += 1) {
       const name = faker.commerce.productName();
@@ -23,18 +22,17 @@ const seedCampaignData = function seedCampaignData() {
         Math.floor(Math.random() * goal) + 100000 : Math.floor(Math.random() * goal);
       const backers = Math.floor(Math.random() * 500);
       const endDate = startTime + Math.floor(Math.random() * 90 + 15) * 86400;
-      const type = Math.floor(Math.random() * TYPES);
+      const type = Math.floor(Math.random() * 15);
 
       data.push(`${name},${desc},${author},${user},${country},${pledge},${goal},${backers},${endDate},${type}`);
     }
   }
   data.push('');
-  console.log(`${dataLimit} in ${new Date().getTime() - currentTime} milliseconds`);
 
   currentTime = new Date().getTime();
-  return fs.writeFile('./seedCampaignData.csv', data.join('\n')) 
+  return fs.writeFile('./seedCampaignsData.csv', data.join('\n'), { 'flag': 'a' }) 
     .then((result) => {
-      console.log(`CAMPAIGN DATA: ${LIMIT} entries in ${new Date().getTime() - start} ms`);
+      console.log(`CAMPAIGN DATA: ${dataLimit} entries in ${new Date().getTime() - startTime} ms`);
     })
     .catch((error) => { 
       console.log(error) 
